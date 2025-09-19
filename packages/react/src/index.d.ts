a381e12a99bfccbe7a9f7b3017b4a8cc75be1a97
littensy <56808540+littensy@users.noreply.github.com>
2025-09-19 13:03:14 -0700

    refactor: localize JSX namespace

--- packages/react/src/index.d.ts
+++ packages/react/src/index.d.ts
@@ -1916,6 +1916,39 @@ declare namespace React {
 		name: string;
 		timestamp: number;
 	}
+
+	namespace JSX {
+		type ElementType = string | React.JSXElementConstructor<any>;
+		interface Element extends React.ReactElement<any, any> {}
+		interface ElementClass extends React.Component<any> {
+			render(): React.ReactNode;
+		}
+		interface ElementAttributesProperty {
+			props: {};
+		}
+		interface ElementChildrenAttribute {
+			children: {};
+		}
+
+		// We can't recurse forever because `type` can't be self-referential;
+		// let's assume it's reasonable to do a single React.lazy() around a single React.memo() / vice-versa
+		type LibraryManagedAttributes<C, P> = C extends
+			| React.MemoExoticComponent<infer T>
+			| React.LazyExoticComponent<infer T>
+			? T extends React.MemoExoticComponent<infer U> | React.LazyExoticComponent<infer U>
+				? ReactManagedAttributes<U, P>
+				: ReactManagedAttributes<T, P>
+			: ReactManagedAttributes<C, P>;
+
+		interface IntrinsicAttributes extends React.Attributes {}
+		interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> {}
+
+		type IntrinsicElements = {
+			[K in Exclude<keyof CreatableInstances, ExcludedClassNames> as Lowercase<K>]: React.InstanceProps<
+				CreatableInstances[K]
+			>;
+		};
+	}
 }
 
 // naked 'any' type in a conditional type will short circuit and union both the then/else branches
@@ -2048,38 +2081,3 @@ type ExcludedClassNames =
 	| "VisualizationMode"
 	| "VisualizationModeCategory"
 	| "WorkspaceAnnotation";
-
-declare global {
-	namespace JSX {
-		type ElementType = string | React.JSXElementConstructor<any>;
-		interface Element extends React.ReactElement<any, any> {}
-		interface ElementClass extends React.Component<any> {
-			render(): React.ReactNode;
-		}
-		interface ElementAttributesProperty {
-			props: {};
-		}
-		interface ElementChildrenAttribute {
-			children: {};
-		}
-
-		// We can't recurse forever because `type` can't be self-referential;
-		// let's assume it's reasonable to do a single React.lazy() around a single React.memo() / vice-versa
-		type LibraryManagedAttributes<C, P> = C extends
-			| React.MemoExoticComponent<infer T>
-			| React.LazyExoticComponent<infer T>
-			? T extends React.MemoExoticComponent<infer U> | React.LazyExoticComponent<infer U>
-				? ReactManagedAttributes<U, P>
-				: ReactManagedAttributes<T, P>
-			: ReactManagedAttributes<C, P>;
-
-		interface IntrinsicAttributes extends React.Attributes {}
-		interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> {}
-
-		type IntrinsicElements = {
-			[K in Exclude<keyof CreatableInstances, ExcludedClassNames> as Lowercase<K>]: React.InstanceProps<
-				CreatableInstances[K]
-			>;
-		};
-	}
-}
